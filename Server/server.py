cab1f40f4a6fd764179ec0b4201320da12a7927d
Emil Wojtaszek <emil@appunite.com>
2015-05-18 12:51:47 +0200

    change py-server file indentation

--- Server/server.py
+++ Server/server.py
@@ -15,69 +15,75 @@ clients = set()
 metadatas = dict()
 
 class DiscoveryClient():
-    connection = None
-    relations = set()
+  connection = None
+  relations = set()
 
-    def __init__(self, c):
-        self.connection = c
+  def __init__(self, c):
+    self.connection = c
 
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
-    def open(self):
-        clients.add(DiscoveryClient(self))
-        return None
+  def open(self):
+    clients.add(DiscoveryClient(self))
 
-    def on_close(self):
-        for client in clients:                
-            if client.connection == self:
-                clients.remove(client)
-                break
+  def on_close(self):
+    for client in clients:                
+      if client.connection == self:
+        clients.remove(client)
+        break
 
-    def on_message(self, msg):
-        payload = json.loads(msg)
+  def on_message(self, msg):
+    payload = json.loads(msg)
 
-        # decompose json
-        body = payload["body"]
-        header = payload["header"]
+    # decompose json
+    body = payload["body"]
+    header = payload["header"]
+    body_id = body.get("id")
 
-        # handle `absence`
-        if header["type"] == "absence":
-            print "Recived `absence` message: %s" % (body["id"])
-            for client in clients:
-                if client.connection == self:
-                    client.relations.remove(body["id"])
+    # handle `absence`
+    if header["type"] == "absence":
+      print "Recived `absence` message: %s" % (body_id)
+      
+      if not body_id in metadatas:
+        return
 
-        # handle `presence`
-        if header["type"] == "presence":
-            print "Recived `presence` message: %s" % (body["id"])
-            payload = json.dumps({"header": {"type": "metadata"}, "body": metadatas[body["id"]]})
-            for client in clients:
-                if client.connection == self:
-                    client.relations.add(body["id"])
+      for client in clients:
+        if client.connection == self:
+          client.relations.remove(body_id)
 
-                    # send metadata user to client
-                    client.connection.write_message(payload, binary=True)
+    # handle `presence`
+    if header["type"] == "presence":
+      print "Recived `presence` message: %s" % (body_id)
+      if not body_id in metadatas:
+        return
+
+      payload = json.dumps({"header": {"type": "metadata"}, "body": metadatas[body_id]})
+      for client in clients:
+        if client.connection == self:
+          client.relations.add(body_id)
+          
+          # send metadata user to client
+          client.connection.write_message(payload, binary=True)
             
-        # handle `metadata`
-        if header["type"] == "metadata":
-            print "Recived `metadata` message: %s" % (body)
-            metadatas[body["id"]] = body
-            payload = json.dumps({"header": {"type": "metadata"}, "body": body})
+    # handle `metadata`
+    if header["type"] == "metadata":
+      print "Recived `metadata` message: %s" % (body)
+            
+      metadatas[body_id] = body
+      payload = json.dumps({"header": {"type": "metadata"}, "body": body})
 
-            for client in clients:
-                client.connection.ws_connection.write_message(payload, binary=True)
-
-
+      for client in clients:
+        client.connection.ws_connection.write_message(payload, binary=True)
 
 
 app = tornado.web.Application([
-    (r'/chat', WebSocketHandler)
+  (r'/chat', WebSocketHandler)
 ])
 
 if __name__ == '__main__':
-    parse_command_line()
+  parse_command_line()
 
-    print "Listening on port %i" % (options.port)
-    app.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+  print "Listening on port %i" % (options.port)
+  app.listen(options.port)
+  tornado.ioloop.IOLoop.instance().start()
 
